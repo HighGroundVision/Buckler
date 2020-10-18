da3559996379f5185f55f622184dfac5b55fbdae
RGBKnights <master@rgbknights.com>
2020-10-18 11:51:55 -0400

    lets try this again...

--- HGV.Buckler.Identity/Startup.cs
+++ HGV.Buckler.Identity/Startup.cs
@@ -56,7 +56,8 @@ namespace HGV.Buckler.Identity
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
-                options.EmitStaticAudienceClaim = true; // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = false;
+                //options.EmitStaticAudienceClaim = true; // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
             })
             .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
             .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
