f02fb3d81c656648e417d62b1f5c6c6c9b0e9cda
RGBKnights <master@rgbknights.com>
2020-10-21 19:17:32 -0400

    Added new page to the sign out process to make the the IDS4 work correctly with the ASP.net Identity

--- HGV.Buckler.Identity/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ HGV.Buckler.Identity/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 
 namespace HGV.Buckler.Identity.Areas.Identity.Pages.Account
 {
@@ -15,21 +19,37 @@ namespace HGV.Buckler.Identity.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly IIdentityServerInteractionService _interaction;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<IdentityUser> signInManager, IIdentityServerInteractionService interaction, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
+            _interaction = interaction;
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync(string logoutId = null)
         {
+            _logger.LogInformation("User logged out.");
+            await _signInManager.SignOutAsync();
+
+            if(string.IsNullOrWhiteSpace(logoutId))
+            {
+                return RedirectToPage("./LogoutSuccess");
+            }
+            else
+            {
+                var request = await _interaction.GetLogoutContextAsync(logoutId);
+                return Redirect(request.PostLogoutRedirectUri);
+            }
         }
 
+        /*
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
-            await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+            await _signInManager.SignOutAsync();
+            
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
@@ -39,5 +59,6 @@ namespace HGV.Buckler.Identity.Areas.Identity.Pages.Account
                 return RedirectToPage();
             }
         }
+        */
     }
 }
---
+++ HGV.Buckler.Identity/Areas/Identity/Pages/Account/LogoutSuccess.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+
+namespace HGV.Buckler.Identity.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutSuccessModel : PageModel
+    {
+
+        public LogoutSuccessModel()
+        {
+        }
+    }
+}
